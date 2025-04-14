6676bb95e7bb5c383ed0b7ca6d513e570a325f92
= <=>
2025-04-14 13:31:28 +0200

    uppercase

--- express/server.js
+++ express/server.js
@@ -4,8 +4,20 @@ const app = express();
 const port = 3000;
 let url = `http://localhost:${port}`;
 
+const toUpperCase = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/my-name/:name", (req, res) => {
+  res.send(
+    `Hello ${toUpperCase(req.params.name)} and has ${
+      req.params.name.length
+    } characters.`
+  );
 });
 
 app.listen(port, () => {
