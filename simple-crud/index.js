cdef43b33dff332682eac1d7d462b9c8f3f8402b
= <=>
2025-04-15 12:59:06 +0200

    data animals and get + post

--- simple-crud/index.js
+++ simple-crud/index.js
@@ -1,14 +1,31 @@
 const port = 5376;
 const baseURL = `http://localhost:${port}`;
+const data = require("./data.json");
 
 const express = require("express");
 const cors = require("cors");
 
+let animals = data.animals;
+
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/animals", (req, res) => {
+  res.send(animals);
+});
+
+app.post("/animals", (req, res) => {
+  animals.push({
+    name: req.body.name,
+    strength: req.body.strength,
+  });
+  res.sendStatus(200);
+  console.log(req.body);
+});
+
+app.get("/animals/:id", (req, res) => {
+  res.send(animals[req.params.id]);
 });
 
 app.listen(port, () => {
