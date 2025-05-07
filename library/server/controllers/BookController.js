83147da515bcec7d6830d19a28bd7dba5e92660b
= <=>
2025-05-07 10:09:38 +0200

    book

--- library/server/controllers/BookController.js
+++ library/server/controllers/BookController.js
@@ -1,48 +1,43 @@
-// const Author = require("../models/Author");
-// const Book = require("../models/Book");
+const Book = require("../models/Book");
 
-// const getBooks = async (req, res) => {
-//   const bookList = await Book.findAll();
-//   res.status(200).send(bookList);
-// };
+const getBooks = async (req, res) => {
+  const bookList = await Book.findAll();
+  res.status(200).send(bookList);
+};
 
-// const createBook = async (req, res) => {
-//   const selectedAuthor = await Author.findOne({
-//     where: { name: req.body.author_name },
-//   });
-//   console.log(selectedAuthor);
-//   const createdBook = await Book.create({
-//     title: req.body.title,
-//     AuthorId: selectedAuthor.id,
-//     length: req.body.length,
-//     description: req.body.description,
-//   });
-//   if (createBook) {
-//     res.status(201).send({ id: createdBook.id });
-//   } else {
-//     res.send(res);
-//   }
-// };
+const createBook = async (req, res) => {
+  const createdBook = await Book.create({
+    title: req.body.title,
+    author: req.body.author,
+    publish_date: req.body.publish_date,
+    stock: req.body.stock,
+  });
+  if (createBook) {
+    res.status(201).send({ id: createdBook.id });
+  } else {
+    res.send(res);
+  }
+};
 
-// const updateBook = async (req, res) => {
-//   const selectedBook = await Book.findByPk(req.params.id);
-//   selectedBook.title = req.body.title;
-//   selectedBook.AuthorId = req.body.author_id;
-//   selectedBook.length = req.body.length;
-//   selectedBook.description = req.body.description;
+const updateBook = async (req, res) => {
+  const selectedBook = await Book.findByPk(req.params.id);
+  selectedBook.title = req.body.title;
+  selectedBook.author = req.body.author;
+  selectedBook.publish_date = req.body.publish_date;
+  selectedBook.stock = req.body.stock;
 
-//   await selectedBook.save();
-//   res.status(200).send({ id: selectedBook.id });
-// };
+  await selectedBook.save();
+  res.status(200).send({ id: selectedBook.id });
+};
 
-// const deleteBook = async (req, res) => {
-//   const selectedBook = await Book.findByPk(req.params.id);
+const deleteBook = async (req, res) => {
+  const selectedBook = await Book.findByPk(req.params.id);
 
-//   await selectedBook.destroy();
-//   res.status(200).send({ id: selectedBook.id });
-// };
+  await selectedBook.destroy();
+  res.status(200).send({ id: selectedBook.id });
+};
 
-// exports.createBook = createBook;
-// exports.getBooks = getBooks;
-// exports.updateBook = updateBook;
-// exports.deleteBook = deleteBook;
+exports.createBook = createBook;
+exports.getBooks = getBooks;
+exports.updateBook = updateBook;
+exports.deleteBook = deleteBook;
--- library/server/index.js
+++ library/server/index.js
@@ -4,14 +4,14 @@ const baseURL = `http://localhost:${port}`;
 const express = require("express");
 const cors = require("cors");
 
-// const booksRouter = require("./routes/BookRoutes");
+const booksRouter = require("./routes/BookRoutes");
 
 const main = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
-  // app.use("/books", booksRouter);
+  app.use("/books", booksRouter);
 
   app.get("/", (req, res) => {
     res.send("It works");
