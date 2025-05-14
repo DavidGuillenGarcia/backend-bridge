593c1cf718ceca0489ca81dc0f12e0a437a52103
= <=>
2025-05-14 13:43:56 +0200

    connection

--- azafran/index.js
+++ azafran/index.js
@@ -2,11 +2,18 @@ const port = 8000;
 
 const cors = require("cors");
 const express = require("express");
+const { Connection } = require("./Connection");
 
-const main = () => {
+const main = async () => {
   const app = express();
   app.use(cors());
   app.use(express.json());
+
+  const db = await Connection();
+  const collection = db.collection("products");
+
+  const first = await collection.findOne();
+  console.log(first);
 
   app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
