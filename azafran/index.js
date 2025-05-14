cc13e680680f0e4809aa39c9089cfd3294d7a528
= <=>
2025-05-14 13:02:21 +0200

    estructure

---
+++ azafran/index.js
@@ -0,0 +1,16 @@
+const port = 8000;
+
+const cors = require("cors");
+const express = require("express");
+
+const main = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+  });
+};
+
+main();
